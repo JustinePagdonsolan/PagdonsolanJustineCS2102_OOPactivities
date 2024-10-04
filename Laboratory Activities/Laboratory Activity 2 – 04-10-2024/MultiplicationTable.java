48d9b6f2049ffcfbeaf56f0d85ecfb6c9fbe8264
JustinePagdonsolan <164887230+JustinePagdonsolan@users.noreply.github.com>
2024-10-04 14:34:57 +0800

    Update MultiplicationTable.java

--- Laboratory Activities/Laboratory Activity 2 – 04-10-2024/MultiplicationTable.java
+++ Laboratory Activities/Laboratory Activity 2 – 04-10-2024/MultiplicationTable.java
@@ -1,1 +1,30 @@
+import java.util.Scanner;
+
+public class MultiplicationTable {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Kindly enter the range of the multiplication table: ");
+        int range = input.nextInt();
+
+        int[][] results = new int[range][range];
+
+        for (int i = 0; i < range; i++) {
+            for (int j = 0; j < range; j++) {
+                results[i][j] = (i + 1) * (j + 1);
+            }
+        }
+
+        System.out.println("The Multiplication Table output:");
+        for (int i = 0; i < range; i++) {
+            for (int j = 0; j < range; j++) {
+                System.out.printf("%4d", results[i][j]);
+            }
+            System.out.println();
+        }
+
+        input.close(); 
+    }
+}
 

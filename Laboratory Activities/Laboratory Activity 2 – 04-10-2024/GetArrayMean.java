aceeba1e2b8d5a03b52c2ac1c2b2bc3cdd7f6306
JustinePagdonsolan <164887230+JustinePagdonsolan@users.noreply.github.com>
2024-10-04 14:12:52 +0800

    Update GetArrayMean.java

--- Laboratory Activities/Laboratory Activity 2 – 04-10-2024/GetArrayMean.java
+++ Laboratory Activities/Laboratory Activity 2 – 04-10-2024/GetArrayMean.java
@@ -1,1 +1,42 @@
+import java.util.Scanner;
+
+public class GetArrayMean {
+
+    
+    public static double getArrayMean(int[] array) {
+        int sum = 0;
+        for (int number : array) {
+            sum += number;
+        }
+        return (double) sum / array.length;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        
+        System.out.print("Kindly state the number of elements: ");
+        int n = scanner.nextInt();
+        
+        int[] numbers = new int[n];
+        
+        
+        for (int i = 0; i < n; i++) {
+            if (i == 0) {
+                System.out.print("Enter 1st element/value: ");
+            } else if (i == 1) {
+                System.out.print("Enter 2nd element/value: ");
+            } else if (i == 2) {
+                System.out.print("Enter 3rd element/value: ");
+            } else {
+                System.out.print("Enter " + (i + 1) + "th element/value: ");
+            }
+            numbers[i] = scanner.nextInt();
+        }
+        
+        
+        double mean = getArrayMean(numbers);
+        System.out.printf("The mean of array is: %.2f\n", mean);
+    }
+}
 
